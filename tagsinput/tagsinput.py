cc2493db1eb4dac1da1a4b345805c33356a0dfa4
martinRenou <martin.renou@gmail.com>
2019-10-31 10:58:17 +0100

    Autocompletion

--- tagsinput/tagsinput.py
+++ tagsinput/tagsinput.py
@@ -10,28 +10,37 @@ Represents a list of tags.
 """
 
 from traitlets import (
-    CaselessStrEnum, Bool, Unicode, List, TraitError, validate
+    Bool, CaselessStrEnum, List, TraitError, Unicode, validate
 )
 
-from ipywidgets import Color, ValueWidget
+from ipywidgets import Color, DOMWidget
 
 from ._frontend import module_name, module_version
 
 
 
-class TagsInputBase(ValueWidget):
+class TagsInputBase(DOMWidget):
     _model_module = Unicode(module_name).tag(sync=True)
     _model_module_version = Unicode(module_version).tag(sync=True)
     _view_module = Unicode(module_name).tag(sync=True)
     _view_module_version = Unicode(module_version).tag(sync=True)
 
     value = List().tag(sync=True)
+    allowed_tags = List().tag(sync=True)
     allow_duplicates = Bool(True).tag(sync=True)
 
     @validate('value')
     def _validate_value(self, proposal):
         if ('' in proposal['value']):
             raise TraitError('The value of a TagsInput widget cannot contain blank strings')
+
+        if len(self.allowed_tags) == 0:
+            return proposal['value']
+
+        for tag_value in proposal['value']:
+            if tag_value not in self.allowed_tags:
+                raise TraitError('Tag value {} is not allowed, allowed tags are {}'.format(tag_value, self.allowed_tags))
+
         return proposal['value']
 
 
