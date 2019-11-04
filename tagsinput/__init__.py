6fc8139da4dae0437306155150f98db70740182e
martinRenou <martin.renou@gmail.com>
2019-11-04 15:20:36 +0100

    Add NumbersInput

--- tagsinput/__init__.py
+++ tagsinput/__init__.py
@@ -4,7 +4,7 @@
 # Copyright (c) QuantStack.
 # Distributed under the terms of the Modified BSD License.
 
-from .tagsinput import TagsInput, ColorsInput  # noqa
+from .tagsinput import TagsInput, ColorsInput, FloatsInput, IntsInput  # noqa
 from ._version import __version__, version_info  # noqa
 
 from .nbextension import _jupyter_nbextension_paths  # noqa
